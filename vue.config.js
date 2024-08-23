d1b534b47b793fc019320a59a9004d5699dbe6c3
LAMMU <chen_yongchao@126.com>
2024-08-24 00:36:00 +0800

    config: vue2子应用跨域配置

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   devServer: {
     port: 1330,
+    host: '0.0.0.0',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    /** 解决webpack项目本地调试 使用自定义域名:1330打开项目报：Invalid Host header✔ */
+    // disableHostCheck: true,
   },
   transpileDependencies: true
 })
