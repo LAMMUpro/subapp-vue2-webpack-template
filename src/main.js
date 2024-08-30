2836f2746f1ac2557fe16f29256fbabba0d6d11a
陈永超 <chen_yongchao@126.com>
2024-08-30 10:11:56 +0800

    chore: 派发组件渲染插槽

--- src/main.js
+++ src/main.js
@@ -5,11 +5,16 @@ import CONSTS from '@/utils/CONSTS';
 import { baseRoutes } from '@/router';
 import { parseRoutesMetaParentComponent } from '@/router/helper';
 import { isSubApp } from 'micro-app-utils';
+import { generateDataListener } from 'micro-app-utils/listener';
+import { MicroComponentSlotMap } from 'micro-app-utils/data';
 
 Vue.use(VueRouter);
 
 let app = undefined;
 let router = undefined;
+
+/** 监听微前端主应用数据, (data: BaseObj<any>) => void */
+let dataListener;
 
 /**
  * 微前端渲染钩子
@@ -31,6 +36,22 @@ window.mount = () => {
 
   /** $mount第二个参数要设置为true，否则挂载节点会被【替换】，导致切换应用会导致挂载id不存在！ */
   app.$mount(appRenderNode, true);
+
+  dataListener = generateDataListener({
+    micro_component: ({ slotName, elementId, props, parentElementId }) => {
+      const elementDom = document.body.querySelector(`#${elementId}`);
+      const slotVNode = MicroComponentSlotMap[parentElementId]?.[slotName];
+
+      if (elementDom && slotVNode) {
+        new Vue({
+          render(h) {
+            return h('div', slotVNode({ ...props }));
+          },
+        }).$mount(elementDom, true);
+      }
+    },
+  });
+  window.microApp?.addDataListener(dataListener, true);
 };
 
 /**
@@ -47,6 +68,8 @@ window.unmount = async () => {
   app.$el.innerHTML = '';
   app = undefined;
   router = undefined;
+
+  window.microApp?.removeDataListener(dataListener);
 }
 
 /**
