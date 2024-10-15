da77e01441e2a287adb1a0580e7d5bc7b792b2cb
LAMMU <chen_yongchao@126.com>
2024-10-15 23:26:38 +0800

    chore: 示例更新

--- src/pages/demo/routeComponent.vue
+++ src/pages/demo/routeComponent.vue
@@ -5,12 +5,10 @@
       <span>ps：同时react18页面使用了派发组件</span>
     </section>
     <MicroApp
-      _prefix="vue2-1-"
       _name="react18"
       _path="/react18/#/home"
     ></MicroApp>
     <MicroApp
-      _prefix="vue2-2-"
       _name="react18"
       _path="/react18/#/demo/micromainComponent"
     ></MicroApp>
@@ -19,7 +17,6 @@
       <span>ps：同时vue3页面使用了派发组件</span>
     </section>
     <MicroApp
-      _prefix="vue2-3-"
       _name="vue3"
       _path="/vue3/#/home"
     ></MicroApp>
