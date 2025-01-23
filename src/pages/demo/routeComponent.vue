73327204372d2c4bc7a3165bdf49b2f099e8869d
陈永超 <chen_yongchao@126.com>
2025-01-23 16:32:51 +0800

    chore: 路由组件demo优化

--- src/pages/demo/routeComponent.vue
+++ src/pages/demo/routeComponent.vue
@@ -1,25 +1,54 @@
 <template>
   <div>
-    <section class="-m-card">
-      <h3 class="-m-title">在vue2中访问react18路由</h3>
-      <span>ps：同时react18页面使用了派发组件</span>
-    </section>
-    <MicroApp
-      _name="react18"
-      _path="/react18/#/home"
-    ></MicroApp>
-    <MicroApp
-      _name="react18"
-      _path="/react18/#/demo/micromainComponent"
-    ></MicroApp>
-    <section class="-m-card">
-      <h3 class="-m-title">在vue2中访问vue3路由</h3>
-      <span>ps：同时vue3页面使用了派发组件</span>
-    </section>
-    <MicroApp
-      _name="vue3"
-      _path="/vue3/#/home"
-    ></MicroApp>
+    <div style="margin-bottom: 10px;">
+      <section class="-m-card lightgray">
+        <h3 class="-m-title">在vue2中访问react18首页</h3>
+      </section>
+      <section style="margin-left: 16px;">
+        <MicroApp
+          _name="react18"
+          _path="/react18/#/home"
+        ></MicroApp>
+      </section>
+    </div>
+    
+    <div style="margin-bottom: 10px;">
+      <section class="-m-card lightgray">
+        <h3 class="-m-title">在vue2中访问react18路由</h3>
+        <span>ps：同时react页面使用了派发组件</span>
+      </section>
+      <section style="margin-left: 16px;">
+        <MicroApp
+          _name="react18"
+          _path="/react18/#/demo/micromainComponent"
+        ></MicroApp>
+      </section>
+    </div>
+
+    <div style="margin-bottom: 10px;">
+      <section class="-m-card lightgray">
+        <h3 class="-m-title">在vue2中访问vue3首页</h3>
+      </section>
+      <section style="margin-left: 16px;">
+        <MicroApp
+          _name="vue3"
+          _path="/vue3/#/home"
+        ></MicroApp>
+      </section>
+    </div>
+    
+    <div style="margin-bottom: 10px;">
+      <section class="-m-card lightgray">
+        <h3 class="-m-title">在vue2中访问vue3路由</h3>
+        <span>ps：同时vue2页面使用了派发组件</span>
+      </section>
+      <section style="margin-left: 16px;">
+        <MicroApp
+          _name="vue3"
+          _path="/vue3/#/demo/micromainComponent"
+        ></MicroApp>
+      </section>
+    </div>
   </div>
 </template>
 
